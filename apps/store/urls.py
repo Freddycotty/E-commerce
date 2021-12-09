1f494f8af4f3745b2494f87378fccd76beaf7fd2
freddycotty <freddycarrillo1912@gmail.com>
2021-12-09 14:40:41 -0400

    [ADD] agregando urls dinamicas en los modelos

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import all_products
+from .views import all_products, product_detail
 app_name = 'store'
 
 urlpatterns = [
     path('', all_products, name = 'all_products'),
+    path("item/<slug:slug>/", product_detail, name="product_detail")
 ]
