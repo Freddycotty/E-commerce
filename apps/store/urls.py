5a4c636a8ae631b234d4dc60baa6dd12084853bd
freddycotty <freddycarrillo1912@gmail.com>
2021-12-13 16:55:13 -0400

    [ADD] agregando configuracion de archivos static

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -4,6 +4,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', all_products, name = 'all_products'),
-    path("item/<slug:slug>/", product_detail, name="product_detail"),
-    path("search/<slug:category_slug>/", category_list, name="category_list"),
+    path("<slug:slug>/", product_detail, name="product_detail"),
+    path("shop/<slug:category_slug>/", category_list, name="category_list"),
 ]
