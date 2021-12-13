59f676c74510b424dd7fef87ad179ee431b83a93
freddycotty <freddycarrillo1912@gmail.com>
2021-12-13 15:40:21 -0400

    [UPDATE] Reconfigurando el context processor

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'apps.store.views.categories',
+                'apps.store.context_processors.categories',
             ],
         },
     },
