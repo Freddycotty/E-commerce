cc61b00fbcbe729f6daaae682cff41e009a7ea3c
freddycotty <freddycarrillo1912@gmail.com>
2021-12-13 14:33:29 -0400

    [ADD] agregando slug a la vista

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import all_products, product_detail
+from .views import all_products, product_detail, category_list
 app_name = 'store'
 
 urlpatterns = [
     path('', all_products, name = 'all_products'),
-    path("item/<slug:slug>/", product_detail, name="product_detail")
+    path("item/<slug:slug>/", product_detail, name="product_detail"),
+    path("search/<slug:category_slug>/", category_list, name="category_list"),
 ]
